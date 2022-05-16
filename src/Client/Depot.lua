20f9762074847b44c7246e24a09fdff7dc144a50
Manuel <manuel.l.soares@tecnico.ulisboa.pt>
2022-05-16 14:29:24 +0100

    AAdded Client Code

---
+++ src/Client/Depot.lua
@@ -0,0 +1,53 @@
+function register()
+    local myURL = "http://localhost:5000/depots"
+    body = textutils.serialiseJSON({location_x = 1,location_y = 1,location_z = 1})
+    headers = {[ "Content-Type" ] = "application/json"}
+    http.request({url=myURL,method="POST",headers=headers, body=body})
+    local event, url, response
+    repeat
+        event, url, response = os.pullEvent("http_success")
+    until url == myURL
+    code, text = response.getResponseCode()
+    if not code == 200 then
+        print(code,text)
+        return -1
+    end
+    local responseTable = textutils.unserialiseJSON(response.readAll())
+    saveIDToFile(responseTable['id'])
+    return tonumber(responseTable['id'])
+end
+
+function unregister()
+    print(ID)
+    print(tostring(math.floor(ID)))
+    local myURL = "http://localhost:5000/depot/" .. tostring(math.floor(ID))
+    http.request({url=myURL,method="DELETE"})
+    local event, url, response
+    repeat
+        event, url, response = os.pullEvent("http_success")
+    until url == myURL
+    fs.delete("ID")
+end
+
+function readIDFromFile()
+    local Handle = fs.open("ID", "r")
+    if Handle == nil then 
+        return -1
+    end
+    local IDString = Handle.read(l)
+    Handle.close()
+    return tonumber(IDString,10)
+end
+
+function saveIDToFile(id)
+    local Handle = fs.open("ID", "w")
+    if Handle == nil then return end
+    Handle.write(tonumber(id))
+    Handle.close()
+end
+
+ID = readIDFromFile()
+if ID == -1 then
+    ID = register()
+    end
+unregister()
